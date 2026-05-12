13fab3b89267d5f614d3201c82c3f319e160c5a0
Rishi <rishibabusoftwaredeveloper@gmail.com>
2026-05-12 11:32:10 +0530

    UC1: Calculate length of a line based on two points using

--- LineComparison.java
+++ LineComparison.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter coordinates for Point 1:");
+        System.out.print("x1: ");
+        double x1 = sc.nextDouble();
+        System.out.print("y1: ");
+        double y1 = sc.nextDouble();
+
+        System.out.println("Enter coordinates for Point 2:");
+        System.out.print("x2: ");
+        double x2 = sc.nextDouble();
+        System.out.print("y2: ");
+        double y2 = sc.nextDouble();
+
+        double length = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+        System.out.println("\nThe length of the line is: " + length);
+        sc.close();
     }
 }
